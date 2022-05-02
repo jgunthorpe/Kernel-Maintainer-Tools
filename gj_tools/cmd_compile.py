1da4f0673928b63b8990c8464657a16bda6f28ae
Jason Gunthorpe <jgg@nvidia.com>
2022-05-02 11:42:15 -0300

    build: Support cross-compiling for 'gj b'
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- gj_tools/cmd_compile.py
+++ gj_tools/cmd_compile.py
@@ -7,14 +7,15 @@ def is_linux():
             and os.path.isfile("Makefile"))
 
 
-def get_linux_compiler(args):
+def get_linux_compiler(args, build_dir):
     """Figure out what the last compiler used to build this tree was and keep
     using it"""
     if args.ccache:
         return config.compiler.split()
-    if not os.path.exists("init/.main.o.cmd"):
+    main_cmd_fn = os.path.join(build_dir, "init/.main.o.cmd")
+    if not os.path.exists(main_cmd_fn):
         return config.compiler.split()[-1:]
-    with open("init/.main.o.cmd") as F:
+    with open(main_cmd_fn) as F:
         ln = F.readline()
         cmd = ln.partition(":=")[2].strip().split(' ')
         for idx, I in enumerate(cmd):
@@ -23,21 +24,65 @@ def get_linux_compiler(args):
     return config.compiler.split()[-1:]
 
 
-def compile_linux(args):
-    cmd = [
-        "make",
-        "CC=%s" % (" ".join(get_linux_compiler(args))), "-j8", "-C",
-        os.path.join(os.getcwd())
-    ]
+def compile_linux_x86(args):
+    tot = os.getcwd()
+    build_dir = tot
+    cmd = ["make", "-C", tot]
+    if os.path.exists(os.path.join(tot, "build-x86")):
+        cmd.append("O=build-x86")
+        build_dir = os.path.join(tot, "build-x86")
+
+    cmd.extend(
+        ["CC=%s" % (" ".join(get_linux_compiler(args, build_dir))), "-j8"])
+
     if args.silent:
         cmd.append("-s")
 
-    if (not os.path.exists("compile_commands.json")
-            and os.path.exists("scripts/clang-tools/gen_compile_commands.py")):
+    compile_cmd_fn = os.path.join(build_dir, "compile_commands.json")
+    if not os.path.exists(compile_cmd_fn):
         cmd.append("all")
         cmd.append("compile_commands.json")
 
     os.execvp(cmd[0], cmd)
+
+
+def tuxmake_linux(args, image, arch, prefix):
+    tot = os.getcwd()
+    build_dir = os.path.join(tot, f"build-{args.arch}")
+    cmd = [
+        "docker", "run", "-u", f"{os.getuid()}:{os.getgid()}", "-ti", "--rm",
+        "-v", f"{tot}:{tot}", image
+    ]
+    cmd.extend([
+        "make", "-C", tot, f"O={os.path.basename(build_dir)}", f"ARCH={arch}",
+        f"CROSS_COMPILE={prefix}", "-j8"
+    ])
+
+    if args.silent:
+        cmd.append("-s")
+
+    os.execvp(cmd[0], cmd)
+
+# See https://tuxmake.org/architectures/
+cross_linux = {
+    "x86":
+    compile_linux_x86,
+    "arm64":
+    lambda args: tuxmake_linux(args,
+                               image="docker.io/tuxmake/arm64_gcc:latest",
+                               arch="arm64",
+                               prefix="aarch64-linux-gnu-"),
+    "s390":
+    lambda args: tuxmake_linux(args,
+                               image="docker.io/tuxmake/s390_gcc:latest",
+                               arch="s390",
+                               prefix="s390x-linux-gnu-"),
+    "ppc64":
+    lambda args: tuxmake_linux(args,
+                               image="docker.io/tuxmake/powerpc_gcc:latest",
+                               arch="powerpc",
+                               prefix="powerpc64le-linux-gnu-"),
+}
 
 # -------------------------------------------------------------------------
 
@@ -50,9 +95,7 @@ def is_rdma_core():
 def compile_rdma_core():
     if not os.path.isdir("build"):
         subprocess.check_call(["./build.sh"])
-    os.execvp(
-        "ninja",
-        ["ninja", "-C", os.path.join(os.getcwd(), "build")])
+    os.execvp("ninja", ["ninja", "-C", os.path.join(os.getcwd(), "build")])
 
 
 # -------------------------------------------------------------------------
@@ -68,13 +111,18 @@ def args_b(parser):
                         action="store_true",
                         help="Silent build",
                         default=False)
+    parser.add_argument("--arch",
+                        action="store",
+                        help="Architecture to build for",
+                        choices=set(cross_linux.keys()),
+                        default="x86")
 
 
 def cmd_b(args):
     """Compile the current source tree properly"""
     with in_directory(git_root()):
         if is_linux():
-            compile_linux(args)
+            cross_linux[args.arch](args)
         elif is_rdma_core():
             compile_rdma_core()
         else:
